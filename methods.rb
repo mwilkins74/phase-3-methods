618aabb4f3be4ca068d9d4732bdff4dbeab78931
Mark <markwilkins86@gmail.com>
2022-06-06 16:13:59 -0400

    commit tree f38d8a06d8f80e910b87f05e8775cb34b7a1b005

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts  "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add = add(2, 5)
+
+def halve(number)
+    return nil unless num.class == integer
+
+    num / 2
+end
+
+
+
